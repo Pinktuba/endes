5cd41da3460c01d9520edf28fbfbe4c3cf9115e4
Noelia Gracia <Tardes T2@A15PC15>
2020-12-04 19:55:34 +0100

    commit new Class Elemento

---
+++ ProyectoPruebasENDES/src/modelo/Elemento.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package modelo;
+
+/**
+ * @author Tardes T2 Gracia
+ * Fecha:  4 dic. 2020
+ * @verison
+ */
+public class Elemento {
+private int numero;
+private String descripcion;
+public Elemento(int numero, String descripcion) {
+	super();
+	this.numero = numero;
+	this.descripcion = descripcion;
+}
+
+
+}
